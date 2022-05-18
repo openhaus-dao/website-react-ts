a72f97cbf20385a8249982f819df86d8f4322023
Sean <seanlengws@gmail.com>
2022-05-18 11:39:06 +0800

    Added mobile support for nav bar

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,30 +1,64 @@
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import {
-  LogoContainer,
-  MainContainer,
-  NavOption,
-  RightContainer,
-  NavContainer,
-  JoinUs,
+    JoinUs,
+    NavOption,
+    RightNavIcon,
+    NavContainer,
+    LogoContainer,
+    MainContainer,
+    RightContainer,
+    RightNavOptionsContainer,
 } from "./Styles/NavBarStyle";
 import { DISCORD, EVENT, NOTION } from "../Constant";
+import useMobile from "../Hooks/useMobile";
+
+interface NavOptionsParams {
+    show: Boolean;
+}
+
+const RenderNavOptions: React.FC<NavOptionsParams> = ({ show }) => {
+    const navigate = useNavigate();
+
+    return (
+        show && (
+            <RightNavOptionsContainer>
+                <RightContainer>
+                    <NavOption onClick={() => navigate("/")}>Home</NavOption>
+                    <NavOption onClick={() => navigate("/about-us")}>About Us</NavOption>
+                    <NavOption href={EVENT}>Events</NavOption>
+                    <NavOption href={NOTION}>Resources</NavOption>
+                    <JoinUs href={DISCORD}>Join Us</JoinUs>
+                </RightContainer>
+            </RightNavOptionsContainer>
+        )
+    );
+};
 
 const NavBar = () => {
-  const navigate = useNavigate();
-  return (
-    <MainContainer>
-      <NavContainer>
-        <LogoContainer onClick={() => navigate("/")}>OpenHaus</LogoContainer>
-        <RightContainer>
-          <NavOption onClick={() => navigate("/")}>Home</NavOption>
-          <NavOption onClick={() => navigate("/about-us")}>About Us</NavOption>
-          <NavOption href={EVENT}>Events</NavOption>
-          <NavOption href={NOTION}>Resources</NavOption>
-          <JoinUs href={DISCORD}>Join Us</JoinUs>
-        </RightContainer>
-      </NavContainer>
-    </MainContainer>
-  );
+    const isMobile = useMobile();
+    const navigate = useNavigate();
+    const [show, setShow] = useState<Boolean>(false);
+
+    useEffect(() => {
+        setShow(!isMobile);
+    }, [isMobile]);
+
+    console.log(isMobile);
+    return (
+        <MainContainer>
+            <NavContainer>
+                <LogoContainer onClick={() => navigate("/")}>OpenHaus</LogoContainer>
+
+                <RightContainer>
+                    <RightNavIcon onClick={() => setShow(isMobile ? !show : true)}>
+                        <img src="/src/Resources/menu.png" width={25} />
+                    </RightNavIcon>
+                    <RenderNavOptions show={show} />
+                </RightContainer>
+            </NavContainer>
+        </MainContainer>
+    );
 };
 
 export default NavBar;
--- src/Components/Styles/NavBarStyle.ts
+++ src/Components/Styles/NavBarStyle.ts
@@ -1,83 +1,114 @@
 import styled from "styled-components";
 
 export const MainContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  padding: 15px 0;
-  border-bottom: 0.2px solid rgba(0, 0, 0, 0.2);
-  height: 40px;
-  width: 100%;
+    display: flex;
+    justify-content: center;
+    padding: 15px 0;
+    border-bottom: 0.2px solid rgba(0, 0, 0, 0.2);
+    height: 40px;
+    width: 100%;
 `;
 
 export const NavContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  width: 80%;
+    display: flex;
+    justify-content: center;
+    width: 80%;
+
+    @media screen and (max-width: 768px) {
+        justify-content: space-between;
+    }
 `;
 
 export const LogoContainer = styled.div`
-  font-size: 24px;
-  font-weight: 600;
-  display: flex;
-  height: 100%;
-  width: content-fit;
-  margin-right: 15px;
-  justify-content: center;
-  cursor: pointer;
-  align-items: center;
-  @media screen and (max-width: 576px) {
-    font-size: 1.15rem;
-  }
-  @media screen and (max-width: 768px) {
-    font-size: 1.05rem;
-  }
+    font-size: 24px;
+    font-weight: 600;
+    display: flex;
+    height: 100%;
+    width: content-fit;
+    margin-right: 15px;
+    justify-content: center;
+    cursor: pointer;
+    align-items: center;
+    @media screen and (max-width: 576px) {
+        font-size: 1.15rem;
+    }
+    @media screen and (max-width: 768px) {
+        font-size: 1.55rem;
+        margin-left: 25px;
+    }
 `;
 
 export const RightContainer = styled.div`
-  display: flex;
-  width: fit-content;
-  justify-content: flex-start;
-  align-items: center;
-  @media screen and (max-width: 576px) {
-    font-size: 1.2rem;
-  }
-  @media screen and (max-width: 768px) {
-    font-size: 1.05rem;
-  }
+    display: flex;
+    width: fit-content;
+    justify-content: flex-start;
+    align-items: center;
+
+    @media screen and (max-width: 576px) {
+        font-size: 1.2rem;
+        display: inline;
+    }
+    @media screen and (max-width: 768px) {
+        font-size: 1.05rem;
+        display: inline;
+    }
+`;
+
+export const RightNavIcon = styled.div`
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    visibility: hidden;
+
+    @media screen and (max-width: 576px) {
+        visibility: visible;
+    }
+
+    @media screen and (max-width: 576px) {
+        visibility: visible;
+    }
+`;
+
+export const RightNavOptionsContainer = styled.div`
+    background-color: white;
+    z-index: 1;
+    padding: 20px;
 `;
 
 export const NavOption = styled.a`
-  display: flex;
-  margin: 20px;
-  font-size: 1.8rem;
-  cursor: pointer;
-  text-decoration: none;
-  color: black;
-  @media screen and (max-width: 576px) {
-    font-size: 1.4rem;
-  }
-  @media screen and (max-width: 768px) {
-    font-size: 1.2rem;
-  }
+    display: flex;
+    margin: 20px;
+    font-size: 1.8rem;
+    cursor: pointer;
+    text-decoration: none;
+    color: black;
+
+    @media screen and (max-width: 576px) {
+        font-size: 1.4rem;
+    }
+    @media screen and (max-width: 768px) {
+        font-size: 1.2rem;
+    }
 `;
 
 export const JoinUs = styled.a`
-  display: flex;
-  background: var(--btn-color);
-  color: white;
-  border-radius: 30px;
-  border: 2px solid transparent;
-  text-decoration: none;
-  transition: background-color 200ms ease-in-out;
-  cursor: pointer;
-  justify-content: flex;
-  font-weight: 600;
-  padding: 1.2rem 2rem;
-  font-size: 1.8rem;
-  @media screen and (max-width: 576px) {
-    font-size: 1.4rem;
-  }
-  @media screen and (max-width: 768px) {
-    font-size: 1.2rem;
-  }
+    display: flex;
+    background: var(--btn-color);
+    color: white;
+    border-radius: 30px;
+    border: 2px solid transparent;
+    text-decoration: none;
+    transition: background-color 200ms ease-in-out;
+    cursor: pointer;
+    justify-content: flex;
+    font-weight: 600;
+    padding: 1.2rem 2rem;
+    font-size: 1.8rem;
+    @media screen and (max-width: 576px) {
+        font-size: 1.4rem;
+    }
+    @media screen and (max-width: 768px) {
+        font-size: 1.2rem;
+    }
 `;
